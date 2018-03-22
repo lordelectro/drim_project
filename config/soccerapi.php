5af511a8edd67841f32e9ee2eb4158bc0e611692
maserejje-nathan <maserejjen@gmail.com>
2018-03-22 20:58:54 +0300

    api initial config

---
+++ config/soccerapi.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'api_token' => 'YOUR_TOKEN_HERE',
+
+    // Determines, if responses with the 'data' attribute will be
+    // stripped of it. Set to true to get body within 'data'
+    'without_data' => false,
+
+];
