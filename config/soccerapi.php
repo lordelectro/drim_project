b6168b1afd0fe1078a77cb781fdca27ffbdbb9e7
maserejje-nathan <maserejjen@gmail.com>
2018-03-23 06:38:30 +0300

    two

--- config/soccerapi.php
+++ config/soccerapi.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'api_token' => 'YOUR_TOKEN_HERE',
+    'api_token' => 'yNJyydSCBquWjPhLmzSejMmyGjwfHP77EkpHnVyHMmKbowWdyPvX3XO7HPr8',
 
     // Determines, if responses with the 'data' attribute will be
     // stripped of it. Set to true to get body within 'data'
