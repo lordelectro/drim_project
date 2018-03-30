48ef6f16691afb3b13a930171d5ac2140c3d67dd
maserejje-nathan <maserejjen@gmail.com>
2018-03-30 09:33:27 +0300

    publish substian

--- resources/views/vendor/boilerplate/plugins/demo.blade.php
+++ resources/views/vendor/boilerplate/plugins/demo.blade.php
@@ -1,13 +1,13 @@
 @extends('boilerplate::layout.index', [
-    'title' => 'Plugins',
-    'subtitle' => 'Demo',
+    'title' => 'Matches',
+    'subtitle' => 'Dashboard',
     'breadcrumb' => ['Plugins']]
 )
 
 @section('content')
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        @include('boilerplate::plugins.demo.select2')
+        @include('boilerplate::plugins.demo.widget')
     </div>
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
         @include('boilerplate::plugins.demo.datepicker')
