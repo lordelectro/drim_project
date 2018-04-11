118b1ae1b00abbea469397a636a65c27c3e7a714
maserejje-nathan <maserejjen@gmail.com>
2018-04-11 15:47:02 +0300

    add files that were missing

---
+++ app/country.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class country extends Model
+{
+    //
+    public $fillable = ['country_name','country_id'];
+}
---
+++ app/odd.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class odd extends Model
+{
+    //
+    public $fillable = ['match_id','odd_date','odd_bookmakers'];
+}
