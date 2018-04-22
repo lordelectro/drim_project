3be4ab8fb99f8359f91cad78cd97c79493f827fd
lordelectro <kwagala.josamu3@gmail.com>
2018-04-22 21:53:15 +0300

    Updated passing variable to receipt

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -7,9 +7,9 @@
 
             <div class="container">
                 <form class="form-inline" method="POST" action="{{ route('admin.games') }}">
-
+<!--
   <a href="{{ route('admin.betslip') }}" class="btn btn-primary btn-block btn-group clearfix">Betslip <span class="betslip--count slip-counter">0</span></a>
-    <a name="more" class="pull-right">&nbsp;</a>
+    <a name="more" class="pull-right">&nbsp;</a> -->
 
 
 
@@ -39,10 +39,13 @@
 
                         <button type="submit" class="btn btn-success">Update</button>
 
-                         <button type="submit" class="btn pull-left" >Place Bet</button>
-                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
+                    
+                             <a href="{{ route('admin.betslip') }}" class="btn btn-primary btn-block btn-group clearfix">Betslip <span class="betslip--count slip-counter">0</span></a>
+                             <a name="more" class="pull-right">&nbsp;</a> 
+                        
+                     
+               
+
 
         </form>
                 </div>
@@ -63,12 +66,14 @@
                         <tr>
                             <th scope="col">Match Date</th>
                             <th scope="col">Match Time</th>
-                            <th scope="col">League Name</th>
-                            <th scope="col">ODDS</th>
+                            <th scope="col">League Name</th>    
                             <th scope="col">Home Team</th>
+                             <th scope="col">ODDS</th>
+                               <th scope="col">Draw</th>
+
+                             <th scope="col">Away Team</th>
                             <th scope="col">ODDS</th>
-                            <th scope="col">Away Team</th>
-                            <th scope="col">Draw</th>
+                          
 
 
                         </tr>
@@ -79,35 +84,49 @@
                             @foreach($match as $ev)
 
                                <!-- <tr --> 
-                                 <tr >
+                                 <tr  >
                             
 <!--starts here-->
                                  <td>{{$ev->match_date}}</td>
                                     <td>{{$ev->match_time}}</td>
                                     <td>{{$ev->league_name}}</td>
-                                    <td data-sortindex="0" onclick="SendToBetslip('KhmabDlNEeiA1AAVXS-e4g', 'Slovacko', {{$ev->odd_1}}, 'Soccer', 'Match Result (1X2)', 'Slovacko v Karvina', '4/7/2018 12:00:00 PM', '5e99192a-4d39-e811-80d4-00155d2f9ee2');"
+                              
+                                    <td>{{$ev->match_hometeam_name}}</td>
+
+                                <td data-sortindex="0" onclick="SendToBetslip('KhmabDlNEeiA1AAVXS-e4g', '{{$ev->match_hometeam_name}}', {{$ev->odd_1}}, 'Soccer', 'Match Result (1X2)', 
+                                    '{{$ev->match_hometeam_name}} vs {{$ev->match_awayteam_name}}', '{{$ev->match_date}}'
+                                , '5e99192a-4d39-e811-80d4-00155d2f9ee2');"
                                     >
                                   
                                     <span>
                                        
-                                        <a href="#" class="btn-sm bettingtd" btn btn-success" >{{$ev->odd_1}}</a> 
+                                        <a href="#" class="btn-sm bettingtd btn btn-success" >{{$ev->odd_1}}</a> 
 
                                     </span>
 
                                 </td>
-                                    <td>{{$ev->match_hometeam_name}}</td>
-                                    <td  data-sortindex="1" onclick="SendToBetslip('KhmabjlNEeiA1AAVXS-e4g', 'Karvina' , {{$ev->odd_2}}, 'Soccer', 'Match Result (1X2)', 'Slovacko v Karvina', '4/7/2018 12:00:00 PM', '5e99192a-4d39-e811-80d4-00155d2f9ee2');"
-                                    >
+
+                                   <td data-sortindex="2" onclick="SendToBetslip('KhmabTlNEeiA1AAVXS-e4g', 'Draw', {{$ev->odd_x}}, 'Soccer', 'Match Result (1X2)', 
+                                        '{{$ev->match_hometeam_name}} vs {{$ev->match_awayteam_name}}',
+                                         '{{$ev->match_date}}', '5e99192a-4d39-e811-80d4-00155d2f9ee2');">
+                                        <span>
+                                            <a href="#" class="btn-sm bettingtd btn btn-info" role="button">{{$ev->odd_x}}</a>
+                                        </span></td>
+
+                                 <td>{{$ev->match_awayteam_name}}</td>
+                                <td  data-sortindex="1" onclick="SendToBetslip('KhmabjlNEeiA1AAVXS-e4g', '{{$ev->match_awayteam_name}}' , {{$ev->odd_2}}, 'Soccer', 'Match Result (1X2)', 
+                                    '{{$ev->match_hometeam_name}} vs {{$ev->match_awayteam_name}}', '{{$ev->match_date}}', '5e99192a-4d39-e811-80d4-00155d2f9ee2');" >
+
                                     <span>
 
                                     
-                                    <a href="#" class="btn btn-success" role="button">{{$ev->odd_2}}</a>
+                                    <a href="#" class="btn-sm bettingtd btn btn-success" role="button">{{$ev->odd_2}}</a>
 
                                     </span>
                                 </td>
-                                    <td>{{$ev->match_awayteam_name}}</td>
-                                    <td data-sortindex="2" onclick="SendToBetslip('KhmabTlNEeiA1AAVXS-e4g', 'Draw', '4.5700', 'Soccer', 'Match Result (1X2)', 'Slovacko v Karvina', '4/7/2018 12:00:00 PM', '5e99192a-4d39-e811-80d4-00155d2f9ee2');">
-                                        <span><a href="#" class="btn btn-info" role="button">{{$ev->odd_x}}</a></span></td>
+
+                                   
+                                 
 
 <!--Ends here-->
                                 </tr>
