fa244d11e4c5324089b6de26015d841f93ed5f2c
maserejje-nathan <maserejjen@gmail.com>
2018-04-24 17:04:39 +0300

    new changes

---
+++ resources/views/backend/betreceipts.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ticket</title>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+</head>
+<body>
+
+	<div class="row">
+
+		@foreach($betreceipts as $br)
+
+		@endforeach
+
+	</div>
+
+
+	</body>
+	<html>
+
+
